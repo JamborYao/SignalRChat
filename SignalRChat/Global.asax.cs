13e9ca2a9b621fb89251c2f21d1df54665f3e1e5
JamborYao <v-jayao@microsoft.com>
2017-01-02 14:15:04 +0800

    add appcmd test

--- SignalRChat/Global.asax.cs
+++ SignalRChat/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,26 @@ namespace SignalRChat
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            try
+            {
+                var windowsDir = Environment.GetEnvironmentVariable("windir");
+                var command = System.IO.Path.Combine(windowsDir, @"System32\inetsrv\appcmd.exe set config / section:system.webserver / serverRuntime / appConcurrentRequestLimit:10000");
+                var command1 = System.IO.Path.Combine(windowsDir, @"System32\inetsrv\appcmd.exe");
+
+                var processStartInfo = new ProcessStartInfo()
+                {
+                    Arguments = "set config / section:system.webserver / serverRuntime / appConcurrentRequestLimit:10000",
+                    FileName = command1,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+
+                Process.Start(processStartInfo);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
